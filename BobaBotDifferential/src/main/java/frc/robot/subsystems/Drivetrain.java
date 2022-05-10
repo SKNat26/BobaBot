e5027433fb1fd4d0181cf99a244c4b6da993ceff
SKNat26 <sknath373@gmail.com>
2022-05-10 01:32:36 -0400

    did some stuff

--- BobaBotDifferential/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ BobaBotDifferential/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,13 +1,11 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import java.util.ArrayList;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
-
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.kauailabs.navx.frc.AHRS;
@@ -17,20 +15,20 @@ import edu.wpi.first.wpilibj.SPI;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import frc.robot.Constants;
 
 
 public class Drivetrain extends SubsystemBase {
   
   //RightMotors
-  private final TalonSRX RMOTOR1;
-  private final TalonSRX RMOTOR2; 
-  private final TalonSRX RMOTOR3;
+  private final WPI_TalonSRX RMOTOR1;
+  private final WPI_TalonSRX RMOTOR2; 
+  private final WPI_TalonSRX RMOTOR3;
 
   //LeftMotors
-  private final TalonSRX LMOTOR1;
-  private final TalonSRX LMOTOR2;
-  private final TalonSRX LMOTOR3;
+  private final WPI_TalonSRX LMOTOR1;
+  private final WPI_TalonSRX LMOTOR2;
+  private final WPI_TalonSRX LMOTOR3;
 
   //Speed Controller Groups
   private final MotorControllerGroup R_MOTORS;
@@ -40,29 +38,43 @@ public class Drivetrain extends SubsystemBase {
   public final DifferentialDrive DIFF_DRIVE;
   public boolean isForward;  
 
-  it // TWIST COEFFICIENT
+  // TWIST COEFFICIENT
   public double twistCoefficient;
 
   public Drivetrain() {
     //Right motors
-    this.RMOTOR1 = new TalonSRX(0);
-    this.RMOTOR2 = new TalonSRX(1);
-    this.RMOTOR3 = new TalonSRX(2);
-    //Left motors
-    this.LMOTOR1 = new TalonSRX(3);
-    this.LMOTOR2 = new TalonSRX(4);
-    this.LMOTOR3 = new TalonSRX(5);
-
+    this.RMOTOR1 = new WPI_TalonSRX(Constants.RM1Port);
+    this.RMOTOR2 = new WPI_TalonSRX(Constants.RM2Port);
+    this.RMOTOR3 = new WPI_TalonSRX(Constants.RM3Port);
     
+    //Left motors
+    this.LMOTOR1 = new WPI_TalonSRX(Constants.LM1Port);
+    this.LMOTOR2 = new WPI_TalonSRX(Constants.LM2Port);
+    this.LMOTOR3 = new WPI_TalonSRX(Constants.LM3Port);
+    
+    //Motors Groups
+    R_MOTORS = new MotorControllerGroup(this.RMOTOR1, this.RMOTOR2, this.RMOTOR3);
+    L_MOTORS = new MotorControllerGroup(this.LMOTOR1, this.LMOTOR2, this.LMOTOR3);
+
+    //Diff drive
+    DIFF_DRIVE = new DifferentialDrive(L_MOTORS, R_MOTORS);
+
+
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  public void configureMotors() {
+    this.RMOTOR1.configFactoryDefault();
+    this.RMOTOR2.configFactoryDefault();
+    this.RMOTOR3.configFactoryDefault();
+    this.LMOTOR1.configFactoryDefault();
+    this.LMOTOR2.configFactoryDefault();
+    this.LMOTOR3.configFactoryDefault();
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    this.RMOTOR1.setNeutralMode(NeutralMode.Coast);
+    this.RMOTOR2.setNeutralMode(NeutralMode.Coast);
+    this.RMOTOR3.setNeutralMode(NeutralMode.Coast);
+    this.LMOTOR1.setNeutralMode(NeutralMode.Coast);
+    this.LMOTOR2.setNeutralMode(NeutralMode.Coast);
+    this.LMOTOR3.setNeutralMode(NeutralMode.Coast);
   }
 }
