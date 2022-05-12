79dc763eec990f45a4fd045dbc0e68a2d6e6a5b0
coddyn <97197585+coddyn@users.noreply.github.com>
2022-05-12 17:58:27 -0400

    Update LED_Controller.java

--- BobaBotDifferential/src/main/java/frc/robot/subsystems/LED_Controller.java
+++ BobaBotDifferential/src/main/java/frc/robot/subsystems/LED_Controller.java
@@ -1,35 +1,33 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
-public class LED_Controller {   
-        private int[] rgb;
-        public LED_Controller(int r, int g, int b){
-            rgb = new int[3];
-            rgb[0] = r;
-            rgb[1] = g;
-            rgb[2] = b;
-        }
-        public int getR(){
-            return rgb[0];
-        }
-        public int getG(){
-            return rgb[1];
-        }
-        public int getB(){
-            return rgb[2];
-        }
-        public static void changeColor(AddressableLED lights, AddressableLEDBuffer buffer, LED_Controller color){
-            for(int i = 0; i < buffer.getLength(); i++){
-                buffer.setRGB(i, color.getR(), color.getG(), color.getB());
-                lights.setData(buffer);
-            }
-        }
-    
-        public static void turnOff(AddressableLED lights, AddressableLEDBuffer buffer){
-            for(int i = 0; i < buffer.getLength(); i++){
-                buffer.setRGB(i, 0, 0, 0);
-            }
-        }
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.extra.led;
+
+public class LEDs extends SubsystemBase{
+  public final PWMSparkMax LEDCONTROLLER;
+
+  public LEDs(){
+    this.LEDCONTROLLER = new PWMSparkMax(Constants.LEDPORT);
+  }
+
+  public void setColor(led color){
+    LEDCONTROLLER.set(color.getValue());
+  }
+
+  public void turnOff(){
+    LEDCONTROLLER.set(0.99);
+  }
+
+    @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
 }
